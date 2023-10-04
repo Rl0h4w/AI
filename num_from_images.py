4edac820e7591f5fdb3e5ba973f2c4f2e8cc1868
ns <Rlohaw@yandex.ru>
2023-10-04 22:10:44 +0300

    comments added

--- num_from_images.py
+++ num_from_images.py
@@ -1,4 +1,5 @@
 import math
+
 import numpy as np
 import tensorflow as tf
 
@@ -16,7 +17,7 @@ class NativeDense:
         self.b = tf.Variable(b_initial_value)
 
     def __call__(self, inputs):
-        return self.activation(tf.matmul(inputs, self.W) + self.b)
+        return self.activation(tf.matmul(inputs, self.W) + self.b)  # Произведение весов с биасом
 
     @property
     def weights(self):
@@ -29,13 +30,16 @@ class NativeSequential:
 
     def __call__(self, inputs):
         x = inputs
-        for layer in self.layers:
+        for layer in self.layers:  # итерация по слоям с вызовом обучающего метода
             x = layer(x)
         return x
 
     @property
     def weights(self):
-        return [layer.weights for layer in self.layers]
+        weights = []
+        for layer in self.layers:
+            weights += layer.weights
+        return weights
 
 
 class BatchGenerator:
@@ -55,27 +59,28 @@ class BatchGenerator:
 
 
 def update_weights(gradients, weights):
-    optimizer = tf.keras.optimizers.SGD(learning_rate=1e-3)
-    optimizer.apply_gradients(zip(gradients, weights))
+    optimizer = tf.keras.optimizers.SGD(learning_rate=1e-3)  # оптимизатор
+    optimizer.apply_gradients(zip(gradients, weights))  # применение к весам
 
 
 def one_training_step(model, images_batch, labels_batch):
     with tf.GradientTape() as tape:
-        predictions = model(images_batch)
-        per_sample_losses = tf.keras.losses.sparse_categorical_crossentropy(y_true=labels_batch, y_pred=predictions)
-        average_loss = tf.reduce_mean(per_sample_losses)
-    gradients = tape.gradient(target=average_loss, sources=model.weights)
-    update_weights(gradients, model.weights)
+        predictions = model(images_batch)  # вызов модели для обучения одной эпохи
+        per_sample_losses = tf.keras.losses.sparse_categorical_crossentropy(y_true=labels_batch,
+                                                                            y_pred=predictions)  # потери в виде вектора
+        average_loss = tf.reduce_mean(per_sample_losses)  # усреднение потерь
+    gradients = tape.gradient(target=average_loss, sources=model.weights)  # вычисление градиента
+    update_weights(gradients, model.weights)  # обновление весов
     return average_loss
 
 
 def fit(model, images, labels, epochs, batch_size=128):
     for epoch_counter in range(epochs):
         print(f"Epoch {epoch_counter}")
-    batch_generator = BatchGenerator(images, labels, batch_size)
+    batch_generator = BatchGenerator(images, labels, batch_size) #генерирование батча
     for batch_counter in range(batch_generator.num_batches):
         images_batch, labels_batch = batch_generator.next()
-        loss = one_training_step(model, images_batch, labels_batch)
+        loss = one_training_step(model, images_batch, labels_batch) #вычисление потерь
         if batch_counter % 100 == 0:
             print(f"loss at batch {batch_counter}: {loss:.2f}")
 
@@ -83,9 +88,9 @@ def fit(model, images, labels, epochs, batch_size=128):
 model = NativeSequential([NativeDense(28 * 28, 512, tf.nn.relu), NativeDense(512, 10, tf.nn.softmax)])
 
 (train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.mnist.load_data()
-train_images = train_images.reshape((6000, 28 * 28))
+train_images = train_images.reshape((60000, 28 * 28))
 train_images = train_images.astype("float32") / 255
-test_images = test_images.reshape((6000, 28 * 28))
+test_images = test_images.reshape((10000, 28 * 28))
 test_images = test_images.astype("float32") / 255
 
 fit(model, train_images, train_labels, 10, 128)
